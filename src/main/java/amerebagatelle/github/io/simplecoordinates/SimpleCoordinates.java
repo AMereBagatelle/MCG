b4afdc0e72c8bdc6d486f40bbdc049f7b6326f3f
AMereBagatelle <brennenputh@gmail.com>
2020-04-09 16:34:13 -0400

    Initial Commit

---
+++ src/main/java/amerebagatelle/github/io/simplecoordinates/SimpleCoordinates.java
@@ -0,0 +1,13 @@
+package amerebagatelle.github.io.simplecoordinates;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+
+@Environment(EnvType.CLIENT)
+public class SimpleCoordinates implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
