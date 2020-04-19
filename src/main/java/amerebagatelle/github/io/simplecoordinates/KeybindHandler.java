8385020b0b439d5a44260d4f4a23c84e368069a2
AMereBagatelle <brennenputh@gmail.com>
2020-04-19 14:05:06 -0400

    Makes different coordinates files for each server/world

--- src/main/java/amerebagatelle/github/io/simplecoordinates/KeybindHandler.java
+++ src/main/java/amerebagatelle/github/io/simplecoordinates/KeybindHandler.java
@@ -1,22 +1,13 @@
 package amerebagatelle.github.io.simplecoordinates;
 
-import amerebagatelle.github.io.simplecoordinates.coordinates.CoordinatesManager;
 import amerebagatelle.github.io.simplecoordinates.gui.screen.CoordinatesScreen;
 import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
 import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.network.MessageType;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class KeybindHandler {
     private static FabricKeyBinding coordinatesMenu;
--- src/main/java/amerebagatelle/github/io/simplecoordinates/mixins/ConnectMixin.java
+++ src/main/java/amerebagatelle/github/io/simplecoordinates/mixins/ConnectMixin.java
@@ -4,19 +4,15 @@ import amerebagatelle.github.io.simplecoordinates.coordinates.CoordinatesManager
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.network.ServerInfo;
 import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
-import net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Environment(EnvType.CLIENT)
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class ConnectMixin {
 
