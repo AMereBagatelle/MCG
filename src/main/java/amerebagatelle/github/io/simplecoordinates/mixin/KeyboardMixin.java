749e889f6e64c4b4b23d016b6da19432f6605c24
AMereBagatelle <themerebagatelle@gmail.com>
2020-06-23 08:55:04 -0400

    Readding keybind

---
+++ src/main/java/amerebagatelle/github/io/simplecoordinates/mixin/KeyboardMixin.java
@@ -0,0 +1,23 @@
+package amerebagatelle.github.io.simplecoordinates.mixin;
+
+import amerebagatelle.github.io.simplecoordinates.gui.screen.CoordinatesScreen;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(Keyboard.class)
+public class KeyboardMixin {
+    @Inject(method = "onKey", at = @At("HEAD"))
+    public void onKey(long window, int key, int scancode, int i, int j, CallbackInfo ci) {
+        if (key == GLFW.GLFW_KEY_Y) {
+            MinecraftClient.getInstance().openScreen(new CoordinatesScreen());
+        }
+    }
+}
