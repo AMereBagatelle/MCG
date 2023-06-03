731e8431f47cecd5e13bc2587c902994d776f925
AMereBagatelle <themerebagatelle@gmail.com>
2023-06-03 07:46:20 -0400

    Improve tests

--- src/test/java/amerebagatelle/github/io/mcg/CoordinateFileTest.java
+++ src/test/java/amerebagatelle/github/io/mcg/CoordinateFileTest.java
@@ -1,8 +1,8 @@
 package amerebagatelle.github.io.mcg;
 
 import amerebagatelle.github.io.mcg.coordinates.Coordinate;
+import amerebagatelle.github.io.mcg.coordinates.CoordinateFile;
 import amerebagatelle.github.io.mcg.coordinates.CoordinateRoot;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,10 +11,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class CoordinateSavingTest {
+public class CoordinateFileTest {
     CoordinateRoot root;
 
     @BeforeEach
@@ -33,28 +32,47 @@ public class CoordinateSavingTest {
         root = new CoordinateRoot(rootPath);
     }
 
+    CoordinateFile getTestFile() {
+        return root.getFile("test.json").orElseThrow();
+    }
+
     @Test()
     void coordinateSaving() {
-        var file = assertDoesNotThrow(() -> root.getFile("test.json").orElseThrow());
+        var file = getTestFile();
 
         Coordinate testSet = new Coordinate("test1", 1, 1, 1, "test2");
         file.addCoordinate(testSet);
+
+        testSet = new Coordinate("test2", 1, 1, 1, "test3");
+        file.addCoordinate(testSet);
+
         assertDoesNotThrow(file::save);
     }
 
     @Test()
+    void coordinateAdding() {
+        var file = getTestFile();
+
+        Coordinate testSet = new Coordinate("test1", 1, 1, 1, "test2");
+        file.addCoordinate(testSet);
+        assertThrows(IllegalArgumentException.class, () -> file.addCoordinate(testSet));
+    }
+
+    @Test()
     void coordinateRemoval() {
-        var file = assertDoesNotThrow(() -> root.getFile("test.json").orElseThrow());
+        var file = getTestFile();
         Coordinate testSet = new Coordinate("test1", 1, 1, 1, "test2");
         file.addCoordinate(testSet);
 
         testSet = new Coordinate("test1", 1, 1, 1, "test2");
         assertTrue(file.removeCoordinate(testSet));
+
+        assertFalse(file.removeCoordinate(testSet));
     }
 
     @Test()
     void coordinateFileDeletion() {
-        var file = assertDoesNotThrow(() -> root.getFile("test.json").orElseThrow());
+        var file = getTestFile();
 
         Coordinate testSet = new Coordinate("test1", 1, 1, 1, "test2");
         file.addCoordinate(testSet);
